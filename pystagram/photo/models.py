e61ee046e1b222f3a1a64af8233d12fda507767f
Kim-Duck-Hyun <ejrgus3@gmail.com>
2016-05-24 02:46:06 +0900

    7class_end

--- pystagram/photo/models.py
+++ pystagram/photo/models.py
@@ -4,13 +4,18 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from django.core.urlresolvers import reverse_lazy
+
 class Photo(models.Model):
 	image_file = models.ImageField(upload_to='%Y/%m/%d')
-	filtered_image_file = models.ImageField(upload_to='static_files/uploaded/filtered/%Y/%m/%d')
-	description = models.TextField(max_length=500, blank=True)
-	created_at = models.DateTimeField(auto_now_add = True)
+	filtered_image_file = models.ImageField(null=True , upload_to='static_files/uploaded/filtered/%Y/%m/%d')
+	discription = models.TextField(max_length=500, blank=True)
+	created_at = models.DateTimeField(auto_now_add = True, auto_now = False)
 
 	def delete(self, *args, **kwargs):
 		self.image_file.delete()
 		self.filtered_image_file.delete()
 		super(Photo, self).delete(*args, **kwargs)
+	
+	def get_absolute_url(self):
+		return reverse_lazy('view_single_photo',kwargs={'photo_id': self.id})
--- pystagram/photo/views.py
+++ pystagram/photo/views.py
@@ -1,8 +1,9 @@
 # coding: utf-8
-
-from django.shortcuts import render, get_object_or_404
+from __future__ import unicode_literals
+from django.shortcuts import ( get_object_or_404,render,redirect,)
 from django.http import HttpResponse
-from .models import Photo
+from photo.models import Photo
+from photo.forms import PhotoEditForm
 
 def single_photo(request, photo_id):
 	photo = get_object_or_404(Photo, pk=photo_id)
@@ -15,4 +16,21 @@ def single_photo(request, photo_id):
 		photo_url = photo.image_file.url
 		)
 	)
-	
+
+def new_photo(request):
+	if request.method =="GET":
+		edit_form = PhotoEditForm()
+	elif request.method == "POST":
+		edit_form = PhotoEditForm(request.POST, request.FILES)
+
+		if edit_form.is_valid():
+			new_photo = edit_form.save()
+
+			return redirect (new_photo.get_absolute_url())
+	return render(
+		request,
+		'new_photo.html',
+		{
+			'form': edit_form,
+		}
+	)	
